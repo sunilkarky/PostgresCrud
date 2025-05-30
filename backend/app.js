1d93140e52745ab706065afd617fbe048c26ed9e
sunilkarky <sunilkarki670@gmail.com>
2025-05-30 15:44:48 +0545

    Production changes in cors

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,9 @@ app.use("/uploads", express.static("uploads"));
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
