565f61fe80540982cc36fade6e9a29595546b886
sunilkarky <sunilkarki670@gmail.com>
2025-05-30 13:27:40 +0545

    DB SSL/tsl ERROR fix

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
 const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize(process.env.POSTGRES_URL);
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 sequelize
   .authenticate()
